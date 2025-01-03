7827e6542901ffc4d00c2c8ed8d140de5caed800
Ojenoba <157749489+Ojenoba@users.noreply.github.com>
2025-01-03 07:51:19 -0600

    Add files via upload

---
+++ first scrap page.py
@@ -0,0 +1,17 @@
+# program to log in my app and display username
+def saved_logins =
+
+
+ = {'burnega': 'Calculate_10', 'olabobo': 'sarokome', 'spark': '1,2bang', 'asake': 'yoruba'}
+for k in saved_logins:
+    captured_logins1 = input('Please enter your username:   ')
+    if captured_logins1 in saved_logins:
+        print('Welcome ' + captured_logins1)
+    else:
+        print('No such user found in our database.')
+        break
+    break
+else:
+    print('Welcome '+ saved_logins[def __getitem__(self, __key:])
+
+
